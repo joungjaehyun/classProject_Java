71df4e136634931d6268064021bf9eb605986e53
joungjaehyun <joungjaehyun1008@gmail.com>
2023-04-07 18:18:24 +0900

    Lecture 2023 4.7

---
+++ javaProject/FirstJava/src/chapter12/FileWriterTest.java
@@ -0,0 +1,28 @@
+package chapter12;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterTest {
+
+	public static void main(String[] args) {
+		
+		String str ="String";
+		char ch = 'A';
+		char [] chraArr = {'B','C'};
+		
+		try {
+			FileWriter out = new FileWriter("D:\\test\\test-text.txt");
+			
+			out.write(str);
+			out.write(ch);
+			out.write(chraArr);
+			
+			out.close();
+			System.out.println("파일에 문자열 입력.");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
